10797046b26b222daeb2edfdbe5de7ea8d32d881
Jose Zalacain <Jose Zalacain@10.11.0.25>
2019-02-02 11:27:58 -0400

    Replace WorkLogic by Prolobjectlink

---
+++ src/main/java/org/prolobjectlink/prolog/jpl7/swi7/SwiProlog7Console.java
@@ -0,0 +1,35 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-jpl7-swi7
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jpl7.swi7;
+
+import org.prolobjectlink.prolog.AbstractConsole;
+import org.prolobjectlink.prolog.PrologConsole;
+
+public class SwiProlog7Console extends AbstractConsole implements PrologConsole {
+
+	public SwiProlog7Console() {
+		super(new SwiProlog7());
+	}
+
+	public static void main(String[] args) {
+		new SwiProlog7Console().run(args);
+	}
+
+}
---
+++ src/main/java/org/prolobjectlink/prolog/jpl7/swi7/SwiProlog7Engine.java
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * prolobjectlink-jpi-jpl7-swi7
+ * %%
+ * Copyright (C) 2019 Prolobjectlink Project
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+package org.prolobjectlink.prolog.jpl7.swi7;
+
+import org.jpl7.JPL;
+import org.jpl7.Query;
+import org.jpl7.Term;
+import org.prolobjectlink.Licenses;
+import org.prolobjectlink.prolog.PrologEngine;
+import org.prolobjectlink.prolog.PrologProvider;
+import org.prolobjectlink.prolog.jpl7.JplEngine;
+
+public final class SwiProlog7Engine extends JplEngine implements PrologEngine {
+
+	SwiProlog7Engine(PrologProvider provider) {
+		super(provider);
+	}
+
+	SwiProlog7Engine(PrologProvider provider, String file) {
+		super(provider, file);
+	}
+
+	public final String getLicense() {
+		return Licenses.LGPL_V3;
+	}
+
+	public final String getVersion() {
+		Term swi = Query.oneSolution("current_prolog_flag(version_data,Swi)").get("Swi");
+		return "" + swi.arg(1) + "." + swi.arg(2) + "." + swi.arg(3) + " (JPL v" + JPL.version_string() + ")";
+	}
+
+	public final String getName() {
+		return "SWI-Prolog";
+	}
+
+}
